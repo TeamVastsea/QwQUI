a48045422d0ca073ea06076a502c6adceb46ec2c
unknown <gaonenggg@outlook.com>
2024-09-26 20:39:42 +0800

    feat: add clean:{dist,deps}

--- packages/components/button/dist/index.mjs
+++
@@ -1,8 +0,0 @@
-// src/button.tsx
-import { Fragment, jsx } from "react/jsx-runtime";
-var Button = () => {
-  return /* @__PURE__ */ jsx(Fragment, { children: "hello world" });
-};
-export {
-  Button
-};
