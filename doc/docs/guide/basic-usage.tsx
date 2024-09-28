2c52f621661d73942ab8e9a7cbae69dcbe1b6dcd
GaoNeng-wWw <gaonenggg@outlook.com>
2024-09-28 21:37:35 +0800

    feat: doc hot load

---
+++ doc/docs/guide/basic-usage.tsx
@@ -0,0 +1,7 @@
+import { Button } from '@qwqui/core';
+
+export default function App() {
+  return (
+    <Button />
+  )
+}
--- doc/rspress.config.ts
+++ doc/rspress.config.ts
@@ -1,5 +1,7 @@
 import * as path from 'path';
 import { defineConfig } from 'rspress/config';
+import { pluginPreview } from "@rspress/plugin-preview";
+import { pluginSass } from '@rsbuild/plugin-sass';
 
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
@@ -15,4 +17,20 @@ export default defineConfig({
       { icon: 'github', mode: 'link', content: 'https://github.com/web-infra-dev/rspress' },
     ],
   },
+  plugins: [pluginPreview()],
+  builderConfig: {
+    // plugins: [
+    //   pluginSass()
+    // ],
+    tools: {
+      rspack: {
+        resolve: {
+          alias: {
+            '@qwqui/core': path.join(__dirname, '../packages/components'),
+            '@qwqui/theme': path.join(__dirname, '../packages/theme/index.scss')
+          },
+        },
+      },
+    }
+  }
 });
