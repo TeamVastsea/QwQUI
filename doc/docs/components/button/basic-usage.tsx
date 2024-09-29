db067d2ff8e754ced53e63d4f81bc9eb9f32b1f9
zrll_ <46812903+zrll12@users.noreply.github.com>
2024-09-30 01:55:07 +0800

    feat: add button radius

--- doc/docs/components/button/basic-usage.tsx
+++ doc/docs/components/button/basic-usage.tsx
@@ -2,6 +2,12 @@ import { Button } from '@qwqui/core';
 
 export default function App() {
   return (
-    <Button>Click Me</Button>
+    <div style={{display: "flex", gap: 10}}>
+      <Button size={"xs"}>Button</Button>
+      <Button size={"sm"}>Button</Button>
+      <Button>Button</Button>
+      <Button size={"lg"}>Button</Button>
+      <Button size={"xl"}>Button</Button>
+    </div>
   )
 }
--- packages/components/button/src/button.tsx
+++ packages/components/button/src/button.tsx
@@ -1,15 +1,33 @@
-import React from 'react';
-import './button.scss';
+import React, {useEffect} from 'react';
+import classes from './button.module.scss';
 
 export const Button = (props: ButtonProps) => {
   const size = props.size || 'md';
+  const getRadius = () => {
+    if (props.radius != undefined) {
+      return props.radius;
+    }
+    
+    if (props.size == 'md' || props.size == 'lg') {
+      return 'lg';
+    }
+    
+    if (props.size == 'xl') {
+      return 'xl'
+    }
+    return 'md';
+  }
 
   return (
-    <div>
+    <div className={classes.root}>
       <button
+        className={classes.button}
         style={{
           height: `var(--button-height-${size})`,
           padding: `0 var(--button-padding-${size})`,
+          borderRadius: `var(--button-radius-${getRadius()})`,
+          fontSize: `var(--button-font-size-${size})`,
+          fontWeight: 600
         }}>
         {props.children}
       </button>
@@ -19,9 +37,9 @@ export const Button = (props: ButtonProps) => {
 
 
 export interface ButtonProps {
-  color?: string,
   variant?: string,
   children: React.ReactNode,
   size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl',
-  radius?: 'xs' | 'sm' | 'md' | 'lg' | 'xl',
+  radius?: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'full'
+  color?: 'red'| 'pink' | 'purple' | 'indigo' | 'blue' | 'green' | 'yellow' | 'gray' | 'white' | 'black',
 }
