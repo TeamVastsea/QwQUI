6bffc72cec23f3249dd91e1f56e3cd14e97e2da5
zrll_ <46812903+zrll12@users.noreply.github.com>
2024-09-29 22:15:03 +0800

    feat: add button height & padding

--- packages/components/button/src/button.tsx
+++ packages/components/button/src/button.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
-import './style.scss'
+import './button.scss';
 
 export const Button = (props: ButtonProps) => {
+  const size = props.size || 'md';
+
   return (
-    <button>
-      {props.children}
-    </button>
-  )
+    <div>
+      <button
+        style={{
+          height: `var(--button-height-${size})`,
+          padding: `0 var(--button-padding-${size})`,
+        }}>
+        {props.children}
+      </button>
+    </div>
+  );
 }
+
 
 export interface ButtonProps {
   color?: string,
   variant?: string,
   children: React.ReactNode,
+  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl',
+  radius?: 'xs' | 'sm' | 'md' | 'lg' | 'xl',
 }
