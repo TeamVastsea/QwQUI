0429a4c5e525bfc17f8b371e8a8479dbbf098477
Snowball_233 <wangrb.qq@outlook.com>
2024-10-02 12:39:00 +0800

    chore: unit test

--- packages/components/center/__test__/center.test.tsx
+++ packages/components/center/__test__/center.test.tsx
@@ -1,6 +1,33 @@
-import * as Rect from 'react';
-import { act, render } from '@testing-library/react'
-import { center } from '..';
-describe('center', () => {
-  it.todo('should define');
-})
+import * as React from 'react';
+import { render } from '@testing-library/react';
+import { Center } from '..';
+
+describe('Center', () => {
+  it('should be defined', () => {
+    const { container } = render(
+      <Center>Centered Content</Center>
+    );
+    expect(container).toBeDefined();
+  });
+
+  it('should render children correctly', () => {
+    const { getByText } = render(
+      <Center>Centered Content</Center>
+    );
+    expect(getByText('Centered Content')).toBeInTheDocument();
+  });
+
+  it('should apply inline-flex when inline is true', () => {
+    const { container } = render(
+      <Center inline>Inline Centered Content</Center>
+    );
+    expect(container.firstChild).toHaveStyle('display: inline-flex');
+  });
+
+  it('should apply flex when inline is false', () => {
+    const { container } = render(
+      <Center>Block Centered Content</Center>
+    );
+    expect(container.firstChild).toHaveStyle('display: flex');
+  });
+});
