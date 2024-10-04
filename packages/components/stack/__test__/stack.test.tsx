87676737cb36a4b4a8d93fe0921edef0032f8b84
GaoNeng-wWw <gaonenggg@outlook.com>
2024-10-04 21:47:26 +0800

    test: stack component test

--- packages/components/stack/__test__/stack.test.tsx
+++ packages/components/stack/__test__/stack.test.tsx
@@ -1,6 +1,16 @@
-import * as Rect from 'react';
-import { act, render } from '@testing-library/react'
-import { stack } from '..';
+import { render } from '@testing-library/react'
+import { Stack } from '..';
 describe('stack', () => {
-  it.todo('should define');
+  it('should define', () => {
+    const wrapper = render(<Stack></Stack>)
+    expect(wrapper).toBeDefined();
+  });
+  it('gap', () => {
+    const wrapper = render(
+      <Stack gap={8}></Stack>
+    )
+    const firstChild = wrapper.container.firstElementChild;
+    const gap = window.getComputedStyle(firstChild).getPropertyValue('--stack-gap');
+    expect(gap).toBe('8px');
+  })
 })
