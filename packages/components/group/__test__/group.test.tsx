d03dda2a468a80583c27b925e62cdf92ae2e9c37
GaoNeng-wWw <gaonenggg@outlook.com>
2024-10-05 21:13:35 +0800

    test(layout): group unit test

--- packages/components/group/__test__/group.test.tsx
+++ packages/components/group/__test__/group.test.tsx
@@ -1,6 +1,17 @@
-import * as Rect from 'react';
-import { act, render } from '@testing-library/react'
-import { group } from '..';
+import { render } from '@testing-library/react'
+import { Group } from '..';
 describe('group', () => {
-  it.todo('should define');
+  it('should define', () => {
+    expect(
+      render(<Group></Group>)
+    ).toBeDefined()
+  });
+  it('gap', () => {
+    const wrapper = render(
+      <Group gap={8}></Group>
+    )
+    const firstChild = wrapper.container.firstElementChild;
+    const gap = window.getComputedStyle(firstChild).getPropertyValue('--group-gap');
+    expect(gap).toBe('8px');
+  })
 })
