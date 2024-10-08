015c3a7bfbc89eda31455537672f3f688a720422
Snowball_233 <wangrb.qq@outlook.com>
2024-10-09 00:24:45 +0800

    chore: add typing

--- packages/components/center/src/center.tsx
+++ packages/components/center/src/center.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import classes from './center.module.scss';
 
-export const Center = (props: CenterProps) => {
+export const Center: React.FC<CenterProps> = (props: CenterProps) => {
   const { children, inline } = props;
   return (
     <div
