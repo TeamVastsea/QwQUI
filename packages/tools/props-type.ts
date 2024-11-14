ce96347f8d67f24ffc0e2a242114efc52bfadd88
GaoNeng-wWw <gaonenggg@outlook.com>
2024-11-14 20:14:54 +0800

    feat(tools): add style props

--- packages/tools/props-type.ts
+++ packages/tools/props-type.ts
@@ -1,11 +1,12 @@
-import { ReactNode } from "react";
+import { CSSProperties, ReactNode } from "react";
 
-export interface DefaultPropType {
+export type DefaultPropType = {
   w: number;
   h: number;
   className: string;
+  style: CSSProperties;
   children: ReactNode;
-}
+} & {}
 
 export type PropsType<T = unknown> = T & DefaultPropType
 export type PropsTypeOptional<T = unknown> = Partial<T & DefaultPropType>
